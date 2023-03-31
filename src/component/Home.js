5f19484add8b2551a0e9fdc5c416b1a7bf7cc906
Oluwadebo <ogunweoluwadebo@gmail.com>
2023-03-31 10:06:42 +0100

    home page header done

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import download from "./asset/download-.png"
 
 const Home = () => {
   return (
     <>
       <div className="container">
-        <div className="row">
-          
+        <div className="header">
+          <div className="row bg-light">
+            <img src={download} className="logo" alt="" />
+            <div className="col-12 stack">
+              <center>
+                <span><b><i>STAFF PORTAL</i></b></span>
+              </center>
+            </div>
+          </div>
+        </div>
+        <div className="menu">
+          <h4 className='sf-menu'>Home</h4>
         </div>
       </div>
     </>
